f25b22a4882e1c4ab347ed703d4f3f28ae7a30c9
Cezar <cezarlantosgmail.com>
2021-12-13 18:21:38 +0100

    sistemato

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-const emailList = ['Paqualino', 'Concetta', 'Ntoni', 'Enzo', 'Mara'];
+//mail
+const emailList = ['Pasqualino@gmail.com', 'Concetta@gmail.com', 'Ntoni@gmail.com', 'Enzo@gmail.com', 'Mara@gmail.com'];
 let flag = false;
 
 let email = prompt('Inserisci la tua email');
 
-for (let i = 0; i < email.length; i++) {
-   if (email === emailList[i]) {
+for (let i = 0; i < emailList.length; i++) {
+   if (email.toLowerCase() === emailList[i].toLowerCase()) {
        flag = true;
    }
 }
@@ -14,3 +15,16 @@ if (flag) {
 } else {
     console.log('Mi dispiace! Lei non può partecipare a questa conversazione!')
 }
+
+//dadi
+let humanNumber = Math.floor(Math.random()*7);
+let computerNumber = Math.floor(Math.random()*7);
+
+console.log(humanNumber);
+console.log(computerNumber);
+
+ if (humanNumber < computerNumber) {
+     console.log('Mi dispiace, hai perso!')
+ } else {
+     console.log('Complimenti, sei un campione!')
+ }
